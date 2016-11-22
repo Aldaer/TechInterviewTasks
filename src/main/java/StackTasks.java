5868e39d4dcdbe93a4619e0f6c33a4e104ee01eb
Artem Lodygin <artem_lodygin@epam.com>
2016-11-22 15:03:41 +0300

    Switch Stack to streams

--- src/main/java/StackTasks.java
+++ src/main/java/StackTasks.java
@@ -1,6 +1,7 @@
 import com.sun.org.apache.xml.internal.utils.IntStack;
 
 import java.util.EmptyStackException;
+import java.util.OptionalInt;
 import java.util.Stack;
 import java.util.stream.IntStream;
 
@@ -21,27 +22,27 @@ class StackTasks {
         if (bracket == ']') return '[';
         if (bracket == ')') return '(';
         if (bracket == '}') return '{';
-        return bracket;
+        return 0;
     }
 
     static boolean checkBalanced(CharSequence input) {
-        Stack<Character> charStack = new Stack<>();
+        IntStack cStack = new IntStack(input.length());
 
-        for (int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
+        final OptionalInt firstNonMatching = input.chars().filter(c -> {
             switch (c) {
-                case '[':
-                case '(':
-                case '{':
-                    charStack.push(c);
-                    break;
                 case ']':
                 case ')':
                 case '}':
-                    if (charStack.empty() || opening(c) != charStack.pop())
-                        return false;
+                    return (cStack.empty() || opening((char) c) != cStack.pop());
+                case '[':
+                case '(':
+                case '{':
+                    cStack.push(c);
+                default:
+                    return false;
             }
-        }
-        return (charStack.empty());
+        }).findFirst();
+
+        return ! firstNonMatching.isPresent() && cStack.empty();
     }
 }
